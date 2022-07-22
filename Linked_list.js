c9d9a4c92b83221e07524c57c081dec252003254
themodernmonk7 <sauravavishek@gmail.com>
2022-07-22 07:33:59 +0530

    Added the prepend method to the linked list class

--- Linked_list.js
+++ Linked_list.js
@@ -9,38 +9,43 @@ class Node {
 }
 
 class LinkedList {
-    constructor() {
-        // When  you instantiate a new linked list and it is empty and head will point at null
-        this.head = null
-        this.size = 0
-    }
+  constructor() {
+    // When  you instantiate a new linked list and it is empty and head will point at null
+    this.head = null
+    this.size = 0
+  }
 
-    // Method: Check if the size is empty
-    isEmpty() {
-        return this.size === 0
-    }
+  // Method: Check if the size is empty
+  isEmpty() {
+    return this.size === 0
+  }
 
-    // Method: Get the size of the linked list
-    getSize() {
-        return this.size
-    }
+  // Method: Get the size of the linked list
+  getSize() {
+    return this.size
+  }
 
-    // Adding an element at the start of the list
-    prepend(value) {
-        // create a new node by instantiating the node class
-        const node = new Node(value)
-        if(this.isEmpty()) {
-            this.head = node
-        } else {
-            node.next = this.head
-            this.head = node
-        }
-        // Update the size to keep track of the number of nodes in the list
-        this.size++
+  // Adding an element at the start of the list
+  prepend(value) {
+    // create a new node by instantiating the node class
+    const node = new Node(value)
+    // 👇 If the list is empty
+    if (this.isEmpty()) {
+      this.head = node
+      // 👇 If the list is not empty
+    } else {
+      node.next = this.head
+      this.head = node
     }
-
+    // Update the size to keep track of the number of nodes in the list
+    this.size++
+  }
 }
 
 const list = new LinkedList()
-console.log('List is empty?', list.isEmpty()); //true
-console.log('List size', list.getSize()); //0
+console.log("List is empty?", list.isEmpty()) //true
+console.log("List size", list.getSize()) //0
+
+list.prepend(10)
+list.prepend(20)
+list.prepend(30)

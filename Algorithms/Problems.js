4abad9c0d66098c0c957fba7b047b937ae60cb3d
themodernmonk7 <sauravavishek@gmail.com>
2022-09-16 08:47:01 +0530

    Algorithms Theory

---
+++ Algorithms/Problems.js
@@ -0,0 +1,15 @@
+/*
+Fibonacci Sequence
+
+Problem: Given a number 'n', find the first 'n' elements of the fibonacci sequence.
+
+In mathematics, the fibonacci sequence in which each number is the sum of the two preceding ones.
+The first two numbers in the sequence are 0 and 1.
+Fibonacci (2) = [0, 1]
+Fibonacci (3) = [0, 1, 1]
+Fibonacci (7) = [0, 1, 1, 2, 3, 5, 8]
+*/
+
+const fibonacciSequence = (n, array) => {
+  const constant = [0, 1]
+}
